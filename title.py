3960db0a9b9205b2a9303d352a8a09fe2ee266e6
Stephen Walloch <saildata03@gmail.com>
2019-02-01 20:24:36 -0600

    Change title screen to green text

--- title.py
+++ title.py
@@ -31,9 +31,12 @@ def display_title_screen():
     # Loop over the text for the intro screen one
     # line at a time
     for line in text:
-        print(line)
+        # Print each line in green text
+        print(Fore.GREEN + line)
         # create random delay between 0 and 1 seconds
         time.sleep(random.random())
 
+    # Change the color back to normal
+    print(Style.RESET_ALL)
     # Pause until the user presses the enter key
     input("Press enter to begin...")
