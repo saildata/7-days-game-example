8661168fcb08396ebb0d32f6a83af8497f3a90d4
Stephen Walloch <saildata03@gmail.com>
2019-02-04 11:45:10 -0600

    move basic to one file instead of separate title.py

--- basic-example/main.py
+++ basic-example/main.py
@@ -10,8 +10,28 @@ import os
 import random
 import time    # add delay between events
 
-# local imports
-from title import display_title_screen
+def display_title_screen():
+    '''
+    Display title screen with delay between typing
+    '''
+    text = '''
+    ███████╗    ██████╗  █████╗ ██╗   ██╗███████╗
+    ╚════██║    ██╔══██╗██╔══██╗╚██╗ ██╔╝██╔════╝
+        ██╔╝    ██║  ██║███████║ ╚████╔╝ ███████╗
+       ██╔╝     ██║  ██║██╔══██║  ╚██╔╝  ╚════██║
+       ██║      ██████╔╝██║  ██║   ██║   ███████║
+       ╚═╝      ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚══════╝
+       '''
+
+    print(text)
+
+    print("An exciting adventure game.")
+
+    # blank line
+    print("")
+
+    #Pause until the user presses the enter key
+    input("Press enter to begin...")
 
 def display_day01():
     '''
